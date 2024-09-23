ae0127df66a49f1b8cd1e83bbf9c65377ea6fdf1
John Langenderfer <John@MacBook-Pro-507.local>
2024-09-23 18:03:15 -0400

    Deploy Flutter Web app to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6350328ebc525d95fa068a1c092130e1",
+const RESOURCES = {"flutter_bootstrap.js": "e07dd54b29ad1149505e8bb0a2f52e26",
 "version.json": "1dc15bf2a142b2032e160383b9aa8740",
 "index.html": "470e477f2ad85c27626221e4411f791c",
 "/": "470e477f2ad85c27626221e4411f791c",
 "main.dart.js": "13d55be9d356c9a906ea2f7b96d7439f",
-"404.html": "fb8583085968010d4bd3cc69980d3a7d",
+"404.html": "8d5b79bc92759bcd846a4607b27bd5d2",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/AssetManifest.json": "9d1ab9a2c8d615596149dd4bbbf15c8e",
 "assets/NOTICES": "4effaa38cb6cf17c0acec78f963e1d32",
